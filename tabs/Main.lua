e0909bacc9d05d30ae84e49ffb315a5d52bc313d
amtra5 at codea-scm <amtra5@codea-scm.aws.mapote.com>
2014-10-22 23:13:44 -0400

    added writing saves and loading saves

--- tabs/Main.lua
+++ tabs/Main.lua
@@ -4,26 +4,39 @@
 function setup()
     --start functions
     
-    function saveData()
-        for k, v in pairs(saveFile) do
-            saveLocalData(k, v)
+    function logic.writeSaveData()
+        for k, v in pairs(logic.saveFile) do
+            saveLocalData("saveFile_"..k, v)
         end
         return true
+    end
+    
+    function logic.readSaveData()
+        local totalData = listLocalData()
+        local saveData = {}
+        for i in #totalData do
+            if string.sub(totalData[i], 1, 9) == "saveFile_" then
+                table.insert(saveData, readLocalData(totalData[i]))
+            end
+        end
+        return saveData
     end
     
     --start hooks
     
     function logic.startGame()
         --takes the player to the game board
+        logic.isInGame = true
     end
     
     --start variables
     
-    saveFile = {}
+    logic.saveFile = {}
+    logic.isInGame = false
     
     --start startup logic
     
-    if readLocalData("hasRanTutorial") == false then
+    if not readLocalData("hasRanTutorial") then
         --checks to see if it is the player's first time playing the gane
         if renderer.runTutorial() == true then
             --checks to see if the tutorial has been successfully run
